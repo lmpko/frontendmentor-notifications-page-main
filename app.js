112eaeb135d80a461d654956ced7f559bf2866ea
lmpko <lmpkoo@gmail.com>
2023-05-18 15:30:36 +0200

    corrected missspel

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import LayouComponent from "./src/LayoutComponent";
+import LayoutComponent from "./src/LayoutComponent";
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<LayouComponent/>)
+root.render(<LayoutComponent/>)
