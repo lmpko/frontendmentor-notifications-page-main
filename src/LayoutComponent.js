886c198856642533af962914e32af7210dddeb0a
lmpko <lmpkoo@gmail.com>
2023-05-18 15:59:53 +0200

    again

--- src/LayoutComponent.js
+++ src/LayoutComponent.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
+import NotificationComponent from './NotificationComponent'
+import { data } from "/data"
 
 const LayoutComponent = () => {
+  
+  const [notificationData, setNotificationData] = useState(data);
+  const unreadData = notificationData.filter((item) => item.status === 'unread');
+  
+  const markAllAsRead = () => {
+    const updatedData = notificationData.map((item) => {
+      return { ...item, status: 'read' };
+    });
+    
+    setNotificationData(updatedData);
+  };
+  
   return (
-    <div>LayoutComponent</div>
+    <section>
+        <div className='notif-panel'>
+          <div>
+            <h1>Notifications</h1>
+            <span className='number-of-new-notifications'>{unreadData.length}</span>
+          </div>
+           
+          <a href="#" className='mark-all-as-read' onClick={markAllAsRead}>Mark all as read</a>
+        </div>
+        <NotificationComponent data={notificationData}/>
+    </section>
   )
 }
 
-export default LayoutComponent
+export default LayoutComponent;
