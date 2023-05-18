bacd9055ad42ba111566fd7343ad2621a37337f9
lmpko <lmpkoo@gmail.com>
2023-05-18 15:47:55 +0200

    updated import react-dom

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import LayoutComponent from "./src/LayoutComponent";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
