c15831392bcc260b49b6f38aae89236f441ed9f9
lmpko <lmpkoo@gmail.com>
2023-05-18 10:44:44 +0200

    layout component created and notification component added into it, and data json created witch data

---
+++ src/LayoutComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NotificationComponent from './NotificationComponent'
+
+const LayouComponent = () => {
+  return (
+    <section>
+        <div>
+            <h1>Notifications</h1>
+            <span>3</span>
+            <span>Mark all as read</span>
+        </div>
+        <NotificationComponent/>
+    </section>
+  )
+}
+
+export default LayouComponent
