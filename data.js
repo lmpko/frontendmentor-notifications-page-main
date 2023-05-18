9a238c019571849c3321caa8e43525f072ff3ab9
lmpko <lmpkoo@gmail.com>
2023-05-18 12:47:35 +0200

    changed json to data.js and images required now i can show images dynamically in react component

---
+++ data.js
@@ -0,0 +1,51 @@
+export const data =[
+    {
+      "name": "Mark Webber",
+      "action": "reacted to your recent post",
+      "post": "My first tournament today!",
+      "timestamp": "1m ago",
+      "avatar" : require("/assets/images/avatar-mark-webber.webp")
+    },
+    {
+      "name": "Angela Gray",
+      "action": "followed you",
+      "timestamp": "5m ago",
+      "avatar": require("/assets/images/avatar-angela-gray.webp")
+    },
+    { 
+      "name": "Jacob Thompson",
+      "action": "has joined your group",
+      "club": "Chess Club",
+      "timestamp": "1 day ago",
+      "avatar": require("/assets/images/avatar-jacob-thompson.webp") 
+    },
+    {
+      "name": "Rizky Hasanuddin",
+      "action": "sent you a private message",
+      "timestamp": "5 days ago",
+      "message" : "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+      "avatar": require("/assets/images/avatar-rizky-hasanuddin.webp") 
+    },
+    {
+      "name": "Kimberly Smith",
+      "action": " commented on your picture",
+      "picture": require("/assets/images/image-chess.webp"),
+      "timestamp": "1 week ago",
+      "avatar": require("/assets/images/avatar-kimberly-smith.webp")
+    },
+    {
+      "name": "Nathan Peterson",
+      "action": "reacted to your recent post",
+      "post": "5 end-game strategies to increase your win rate",
+      "timestamp": "2 weeks ago",
+      "avatar": require("/assets/images/avatar-nathan-peterson.webp")
+    },
+    {
+      "name": "Anna Kim",
+      "action": "left the group",
+      "club": "Chess Club",
+      "timestamp": "2 weeks ago",
+      "avatar": require("/assets/images/avatar-anna-kim.webp")
+    }
+  ]
+  
